ff621ba761f0d3e1f700e0d9a2c2887af52f1675
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-12-22 11:40:20 +0300

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View,KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+
+export default function LoginScreen() {
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior='padding'>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder='E mail adresinizi Giriniz'></TextInput>
+        <TextInput style={styles.input} placeholder='Şifreyi Giriniz'></TextInput>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+             <Text style={styles.buttonText}>Giriş Yap</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button,styles.outlineButton]}>
+          <Text style={styles.outlineButtonText}>Kayıt Ol</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer:{},
+  input:{},
+  buttonContainer:{},
+  button:{},
+  buttonText:{},
+  outlineButton:{},
+  outlineButtonText:{}
+});
